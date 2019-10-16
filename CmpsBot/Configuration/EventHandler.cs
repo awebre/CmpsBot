08cef37aef8048c9ca09ae39c34ba3fadfba583c
Jake <jacob.held@selu.edu>
2019-10-16 17:57:55 -0500

    cleaned up code

--- CmpsBot/Configuration/EventHandler.cs
+++ CmpsBot/Configuration/EventHandler.cs
@@ -11,21 +11,10 @@ namespace CmpsBot.Configuration
     public class EventHandler
     {
         private readonly DiscordSocketClient discord;
-        private readonly CommandService commands;
-        private readonly IConfigurationRoot config;
-        private readonly IServiceProvider provider;
 
-        public EventHandler(
-            DiscordSocketClient discord,
-            CommandService commands,
-            IConfigurationRoot config,
-            IServiceProvider provider)
+        public EventHandler(DiscordSocketClient discord)
         {
             this.discord = discord;
-            this.commands = commands;
-            this.config = config;
-            this.provider = provider;
-
             this.discord.UserJoined += NewMemberPrompt;
         }
 
@@ -33,8 +22,8 @@ namespace CmpsBot.Configuration
         {
             var guild = user.Guild;
             var channel = guild.DefaultChannel;
-            await channel.SendMessageAsync($"Welcome, {user.Mention}! Please change your nickname in the server to your first and last name." +
-                $" Please also select your major with an emote reaction.");
+            await channel.SendMessageAsync($"Welcome, {user.Mention}!\nPlease change your nickname in the server to your first and last name." +
+                $" \nAlso, select your major with an emote reaction.");
         }
     }
 }
