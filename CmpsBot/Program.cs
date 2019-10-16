2d513489dd4744ec726fc15ad1fba422415b65a6
Austin Webre <austin.webre@gmail.com>
2019-10-15 20:32:24 -0500

    chore: whoops

--- CmpsBot/Program.cs
+++ CmpsBot/Program.cs
@@ -7,43 +7,8 @@ using Microsoft.Extensions.Configuration;
 
 namespace CmpsBot
 {
-    class Program
+    public class Program
     {
-        private DiscordSocketClient client;
-        private IConfiguration config;
-        
-        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-
-        public async Task MainAsync()
-        {
-            config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile("appsettings.dev.json", true, true)
-                .Build();
-
-            client = new DiscordSocketClient();
-            client.Log += Log;
-
-            await client.LoginAsync(TokenType.Bot, config["discordToken"]);
-            await client.StartAsync();
-            
-            client.MessageReceived += MessageReceived;
-
-            await Task.Delay(-1);
-        }
-        
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-        
-        private async Task MessageReceived(SocketMessage message)
-        {
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
-        }
+        public static void Main(string[] args) => Startup.RunAsync(args).GetAwaiter().GetResult();
     }
 }
